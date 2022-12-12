3fafbfa6d5152671f76c931c3c79049633e979a1
Necelentano <necelentano@gmail.com>
2022-12-13 00:39:41 +0200

    Added isError flag to the response

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -20,7 +20,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(status);
 
-  res.json({ message: err.message });
+  res.json({ message: err.message, isErrorL true });
 };
 
 module.exports = errorHandler;
