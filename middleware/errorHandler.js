e175d7c2643be838094e4dc65a910e9de7785dde
Necelentano <necelentano@gmail.com>
2022-12-15 21:20:34 +0200

    Typo hot fix

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -20,7 +20,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(status);
 
-  res.json({ message: err.message, isErrorL true });
+  res.json({ message: err.message, isError: true });
 };
 
 module.exports = errorHandler;
